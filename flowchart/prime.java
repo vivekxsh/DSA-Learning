5d72d02384d7adc3b3a4182c2ad1f0d23534cc1b
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-03 22:49:09 +0530

    prime number

---
+++ flowchart/prime.java
@@ -0,0 +1,26 @@
+package flowchart;
+
+import java.util.Scanner;
+
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int div = 2;
+
+        while (div < n) {
+            if (n % div == 0) {
+                System.out.println(n + " is not a prime number.");
+                return;
+            }
+            div++;
+
+        }
+
+        System.out.println(n + " is a prime number.");
+        sc.close();
+
+    }
+}
