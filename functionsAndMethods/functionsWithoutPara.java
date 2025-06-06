9edc59047f74af43bcb455f21f5d0f8f6b4bb490
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-06 18:08:36 +0530

    Create functionWithourPara.java

---
+++ functionsAndMethods/functionsWithoutPara.java
@@ -0,0 +1,17 @@
+package functionsAndMethods;
+
+public class functionsWithoutPara {
+
+    public static void printHello() {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+
+        // Calling the function
+        printHello();
+
+    }
+}
