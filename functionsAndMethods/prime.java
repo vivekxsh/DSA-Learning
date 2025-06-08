f6286a7905abc395d71f13f7efcd6e46e9ef240f
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-08 16:51:35 +0530

    Create prime.java

---
+++ functionsAndMethods/prime.java
@@ -0,0 +1,29 @@
+package functionsAndMethods;
+
+public class prime {
+
+    // Function to check if a number is prime
+    // A prime number is a natural number greater than 1 that cannot be formed by
+    // multiplying two smaller natural numbers.
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 29;
+        if (isPrime(n)) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+    }
+}
