4e5433d9eb59b1046b53c2a64c212b06a4f05e29
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-09 22:54:25 +0530

    Create arrayCreation.java

---
+++ Arrays/arrayCreation.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class arrayCreation {
+    public static void main(String[] args) {
+        // Array declaration and initialization
+        int arr[] = { 21, 22, 23, 24, 25 };
+        System.out.println(arr[0]);
+        // int arr[] = new int[5]; // This creates an array of size 5 with default
+        // values 0
+        int arr2[] = new int[5];
+        System.out.println(arr2[0]);
+
+        String str[] = { "mango", "apple", "banana", "orange", "grape" };
+        System.out.println(str[0]);
+    }
+}
