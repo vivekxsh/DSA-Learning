2a7ff762af01c48a2d3db27e8bb3e233c83b4d6c
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-09 23:40:26 +0530

    Create linearSearch.java

---
+++ Arrays/linearSearch.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class linearSearch {
+    public static void main(String[] args) {
+        int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
+        int key = 10;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Element found at index: " + i);
+                break; // Exit the loop once the element is found
+            }
+        }
+    }
+}
