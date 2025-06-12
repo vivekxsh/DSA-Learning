2eb86bd26ab3cf478925bb5c2d5025548716995a
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-12 20:54:51 +0530

    Update maxSubSum.java

--- Arrays/maxSubSum.java
+++ Arrays/maxSubSum.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class maxSubSum {
 
-    public static void maxSum(int[] arr) {
+    public static void sum(int[] arr) {
         int n = arr.length;
-        int sum = Integer.MIN_VALUE;
+        int maxSum = Integer.MIN_VALUE;
 
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -14,27 +14,27 @@ public class maxSubSum {
                 for (int k = i; k <= j; k++) {
                     currentSum += arr[k];
                 }
-                System.out.println("current sum: " + currentSum);
-                if (sum < currentSum) {
-                    sum = currentSum;
+                System.out.println("Current sum: " + currentSum);
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
                 }
             }
         }
 
-        System.out.println("Maximum subarray sum is: " + sum);
+        System.out.println("Maximum subarray sum is: " + maxSum);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of elements in the array: ");
         int n = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.print("Enter the elements of the array: ");
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        maxSum(arr);
+        sum(arr);
 
         sc.close();
     }
