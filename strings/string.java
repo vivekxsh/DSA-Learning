fb6c041c74df3d49f801fb78f72e02cec7a21c26
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-20 22:59:27 +0530

    Create string.java

---
+++ strings/string.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class string {
+    public static void main(String[] args) {
+        char arr[] = { 'a', 'b', 'c', 'd' };
+
+        String str = new String(arr);
+
+        System.out.println(str);
+
+    }
+}
