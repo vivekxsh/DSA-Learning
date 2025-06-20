11a73187166c40d79fb5f2c97646c29a44b0a24e
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-20 23:02:02 +0530

    Update string.java

--- strings/string.java
+++ strings/string.java
@@ -1,5 +1,7 @@
 package strings;
 
+import java.util.Scanner;
+
 public class string {
     public static void main(String[] args) {
         char arr[] = { 'a', 'b', 'c', 'd' };
@@ -7,6 +9,14 @@ public class string {
         String str = new String(arr);
 
         System.out.println(str);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Get the string input from user : ");
+        String s = sc.next();
+
+        System.out.println(s);
+
+        sc.close();
 
     }
 }
