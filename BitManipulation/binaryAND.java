33b16b14a85eee4f656c5bac8b6c4817057c349b
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-22 11:27:49 +0530

    Create binaryAND.java

---
+++ BitManipulation/binaryAND.java
@@ -0,0 +1,7 @@
+package BitManipulation;
+
+public class binaryAND {
+    public static void main(String[] args) {
+        System.out.println("The AND(&) operation of 5 and 6 is : " + (5 & 6));
+    }
+}
