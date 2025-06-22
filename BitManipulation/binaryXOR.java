54188dfa1fa03de378319459935cdd620a30a771
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-22 11:42:36 +0530

    Create binaryXOR.java

---
+++ BitManipulation/binaryXOR.java
@@ -0,0 +1,7 @@
+package BitManipulation;
+
+public class binaryXOR {
+    public static void main(String[] args) {
+        System.out.println("The XOR(^) operation of two numbers : " + (5 ^ 6));
+    }
+}
