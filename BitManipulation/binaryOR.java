4d38b215a0b1c75665e3fd0bb011829af256872b
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-22 11:36:29 +0530

    Create binaryOR.java

---
+++ BitManipulation/binaryOR.java
@@ -0,0 +1,7 @@
+package BitManipulation;
+
+public class binaryOR {
+    public static void main(String[] args) {
+        System.out.println("The binary OR( | ) operation on two number resulting in : " + (5 | 6));
+    }
+}
