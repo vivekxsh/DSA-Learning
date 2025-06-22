1aabdfa42a1d25db95c51eedd5b2e0c873f33768
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-22 14:38:21 +0530

    Create binaryRightShift.java

---
+++ BitManipulation/binaryRightShift.java
@@ -0,0 +1,12 @@
+package BitManipulation;
+
+public class binaryRightShift {
+    public static void main(String[] args) {
+        System.out.println("Right shift ( >> ) operator --> " + (7 >> 1));
+
+        /*
+         * Formula
+         * a >> b = a/(2)^b
+         */
+    }
+}
