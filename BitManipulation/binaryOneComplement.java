0eb4b8ab21fac4960d0a8afbf85fa115551a86dc
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-22 11:56:18 +0530

    Create binaryOneComplement.java

---
+++ BitManipulation/binaryOneComplement.java
@@ -0,0 +1,8 @@
+package BitManipulation;
+
+public class binaryOneComplement {
+    public static void main(String[] args) {
+        System.out.println((~0)); // result = -1
+        System.out.println((~5)); // result = -6
+    }
+}
