684bd478f6ba478ccd161c05a1c35cb9927d1270
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-06-22 12:06:57 +0530

    Create binaryLeftShift.java

---
+++ BitManipulation/binaryLeftShift.java
@@ -0,0 +1,12 @@
+package BitManipulation;
+
+public class binaryLeftShift {
+    public static void main(String[] args) {
+        System.out.println("Binary left shift operator: " + (5 << 2));
+
+        /*
+         * Formula for left shift
+         * a << b = (a * 2^b)
+         */
+    }
+}
