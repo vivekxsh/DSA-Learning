3e587f3abaa5db1723bdbeaa94506f28eb8e5f7f
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-07-09 00:33:48 +0530

    Create syntax.java

---
+++ Arraylist/syntax.java
@@ -0,0 +1,20 @@
+package Arraylist;
+
+import java.util.ArrayList;
+
+public class syntax {
+    public static void main(String[] args) {
+
+        // className objectname = new className;
+        ArrayList<Integer> list1 = new ArrayList<>();
+
+        ArrayList<String> list2 = new ArrayList<>();
+
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        list1.clear();
+        list2.clear();
+        list3.clear();
+
+    }
+}
