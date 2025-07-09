5d5a360e1a1a6820059253a74b35cb87a504775a
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-07-09 23:37:45 +0530

    Update findMaximum.java

--- Arraylist/findMaximum.java
+++ Arraylist/findMaximum.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 
 public class findMaximum {
 
-    public static void main(String args) {
+    public static void main(String[] args) {
 
         // arraylist data = 2 5 9 3 6
 
@@ -15,6 +15,7 @@ public class findMaximum {
         // ArrayList creation
         ArrayList<Integer> arr = new ArrayList<>();
 
+        System.out.println("Enter values: ");
         for (int i = 0; i < 5; i++) {
             arr.add(sc.nextInt());
         }
@@ -22,6 +23,16 @@ public class findMaximum {
         // *** Method 1 (Using inbuilt functions) ***
         Collections.sort(arr);
         System.out.println("The maximum number is: " + arr.get(arr.size() - 1));
+
+        // *** Method 2 (Manually)***
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.size(); i++) {
+            if (max < arr.get(i)) {
+                max = arr.get(i);
+            }
+        }
+
+        System.out.println("Maximum number: " + max);
 
         sc.close();
 
