6515e993044d2a018b203b32e17e4c08ecbafe75
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-07-24 10:44:13 +0530

    Create linkedlist_Collections.java

---
+++ LinkedList/linkedlist_Collections.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class linkedlist_Collections {
+    public static void main(String[] args) {
+
+        // creation
+        LinkedList<Integer> linkedlist = new LinkedList<>();
+
+        // adding
+        linkedlist.add(1);
+        linkedlist.add(2);
+        linkedlist.add(3);
+        linkedlist.add(4);
+        linkedlist.add(5);
+
+        // printing
+        System.out.println(linkedlist);
+
+    }
+}
