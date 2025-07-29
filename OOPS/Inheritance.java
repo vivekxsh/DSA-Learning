9210a8c1206b7ac4d869260934628eb49231a957
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-07-29 09:46:50 +0530

    Create Inheritance.java

---
+++ OOPS/Inheritance.java
@@ -0,0 +1,32 @@
+package OOPS;
+
+// base class / parent class
+class Animal {
+    String skinColor;
+
+    void eat() {
+        System.out.println("Animals do eat.");
+    }
+
+    void breathe() {
+        System.out.println("Animals do breathe.");
+    }
+}
+
+// derived class / subclass
+class Fish extends Animal {
+    int fins;
+
+    void swim() {
+        System.out.println("swim in water.");
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+
+        Fish f1 = new Fish();
+        f1.eat();
+
+    }
+}
