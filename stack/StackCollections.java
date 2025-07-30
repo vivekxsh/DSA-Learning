8cf6df70bc9970e82f86e98867ecb5a85b76d894
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-07-30 22:05:30 +0530

    Create StackCollections.java

---
+++ stack/StackCollections.java
@@ -0,0 +1,18 @@
+package stack;
+
+import java.util.Stack;
+
+public class StackCollections {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+
+        st.push(1);
+        st.push(2);
+        st.push(3);
+
+        while (!st.isEmpty()) {
+            System.out.println(st.peek());
+            st.pop();
+        }
+    }
+}
