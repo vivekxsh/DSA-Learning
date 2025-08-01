7134d574314578dc90e212592743a2775b42294e
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-08-01 21:44:31 +0530

    Update NextGreaterElement.java

--- stack/Questions/NextGreaterElement.java
+++ stack/Questions/NextGreaterElement.java
@@ -1,8 +1,30 @@
 package stack.Questions;
 
-// import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class NextGreaterElement {
+
+    public static int[] nextGreater(int greater[]) {
+
+        int nGreater[] = new int[greater.length];
+
+        Stack<Integer> findGreater = new Stack<>();
+
+        for (int i = greater.length - 1; i >= 0; i--) {
+            int element = greater[i];
+            while (!findGreater.isEmpty() && findGreater.peek() <= element) {
+                findGreater.pop();
+            }
+
+            nGreater[i] = findGreater.isEmpty() ? -1 : findGreater.peek();
+
+            findGreater.push(element);
+        }
+
+        return nGreater;
+    }
+
     public static void main(String[] args) {
         // int elements[] = { 6, 8, 0, 1, 3 };
 
@@ -40,5 +62,23 @@ public class NextGreaterElement {
          * sc.close();
          * 
          */
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the array: ");
+        int n = sc.nextInt();
+        System.out.println("Enter elements in array: ");
+        int greater[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            greater[i] = sc.nextInt();
+        }
+
+        int nGreater[] = nextGreater(greater);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print((i < n - 1) ? nGreater[i] + ", " : nGreater[i]);
+        }
+
+        sc.close();
+
     }
 }
