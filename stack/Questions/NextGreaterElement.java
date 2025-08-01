745320defda6a0c7b50972c4b436e75386b08e49
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-08-01 12:05:29 +0530

    Create NextGreaterElement.java

---
+++ stack/Questions/NextGreaterElement.java
@@ -0,0 +1,44 @@
+package stack.Questions;
+
+// import java.util.Scanner;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        // int elements[] = { 6, 8, 0, 1, 3 };
+
+        /*
+         * 
+         * Approach 1 -> Brute force approach
+         * 
+         * Scanner sc = new Scanner(System.in);
+         * System.out.print("Enter the size of the elements array: ");
+         * int n = sc.nextInt();
+         * System.out.print("Enter elements in elements array: ");
+         * int elements[] = new int[n];
+         * for (int i = 0; i < n; i++) {
+         * elements[i] = sc.nextInt();
+         * }
+         * 
+         * int nextGreater[] = new int[n];
+         * for (int i = 0; i < elements.length; i++) {
+         * for (int j = i + 1; j < elements.length; j++) {
+         * if (elements[i] < elements[j]) {
+         * nextGreater[i] = elements[j];
+         * break;
+         * } else {
+         * nextGreater[i] = -1;
+         * }
+         * }
+         * }
+         * nextGreater[n - 1] = -1;
+         * 
+         * for (int i = 0; i < n; i++) {
+         * System.out.print((i < n - 1) ? nextGreater[i] + ", " : nextGreater[i]);
+         * }
+         * System.out.println();
+         * 
+         * sc.close();
+         * 
+         */
+    }
+}
