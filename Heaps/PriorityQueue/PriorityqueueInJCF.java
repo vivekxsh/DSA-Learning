eae6777893f7cc7b9941c0ec0b3c6b11a0ec7c37
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-08-06 12:46:55 +0530

    Create PriorityQueueJCF.java

---
+++ Heaps/PriorityQueue/PriorityqueueInJCF.java
@@ -0,0 +1,23 @@
+package Heaps.PriorityQueue;
+
+// import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PriorityqueueInJCF {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(3); // O(logn)
+        pq.add(4);
+        pq.add(2);
+        pq.add(7);
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek()); // O(1)
+            pq.poll(); // O(logn)
+
+        }
+    }
+}
