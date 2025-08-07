e709c0f195cdc061d8c03f619e007bf4007d8e98
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-08-07 23:31:07 +0530

    Create PriorityQueueObjects.java

---
+++ Heaps/PriorityQueue/PriorityQueueObjects.java
@@ -0,0 +1,36 @@
+package Heaps.PriorityQueue;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PriorityQueueObjects {
+    static class Student implements Comparable<Student> { // overriding
+        String name;
+        int rank;
+
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(new Student("A", 4));
+        pq.add(new Student("B", 5));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 12));
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek().name + " -> " + pq.peek().rank);
+            pq.remove();
+        }
+
+    }
+}
