81e7fecea92e0d9f5a17069a87ef77bad139bc12
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-08-15 04:13:03 +0530

    Create binaryTree.java

---
+++ BinaryTree/binaryTree.java
@@ -0,0 +1,41 @@
+package BinaryTree;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        this.left = this.right = null;
+    }
+}
+
+public class binaryTree {
+
+    Node root;
+
+    // In-order Traversal
+    public void inorder(Node node) {
+        if (node == null)
+            return;
+        inorder(node.left);
+        System.out.print(node.data + " ");
+        inorder(node.right);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Binary tree");
+
+        binaryTree tree = new binaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.right = new Node(6);
+
+        System.out.println("In-order traversal");
+        tree.inorder(tree.root);
+        System.out.println();
+    }
+}
