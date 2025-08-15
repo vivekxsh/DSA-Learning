8c6703b5c0ca72dd658821b2535cb029e91481f2
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-08-15 06:22:58 +0530

    Create CountOfNodes.java

---
+++ BinaryTree/CountOfNodes.java
@@ -0,0 +1,39 @@
+package BinaryTree;
+
+public class CountOfNodes {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    public static int count(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lc = count(root.left);
+        int rc = count(root.right);
+
+        return lc + rc + 1;
+    }
+
+    public static void main(String args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(count(root));
+    }
+}
