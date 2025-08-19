141d4c98b0c0703758a78e1e366a1f09033ba4e9
Vivek Sharma <118986548+vivekxsh@users.noreply.github.com>
2025-08-19 11:28:00 +0530

    Create InsertionInBST.java

---
+++ BinarySearchTree_BST/InsertionInBST.java
@@ -0,0 +1,57 @@
+package BinarySearchTree_BST;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        this.left = this.right = null;
+    }
+}
+
+public class InsertionInBST {
+
+    static Node insert(Node node, int data) {
+
+        if (node == null) {
+            return new Node(data);
+        }
+
+        // do not allow duplicate values
+        if (node.data == data) {
+            return node;
+        }
+
+        if (data < node.data) {
+            node.left = insert(node.left, data);
+        } else {
+            node.right = insert(node.right, data);
+        }
+        return node;
+    }
+
+    static void inOrder(Node node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.print(node.data + " ");
+            inOrder(node.right);
+        }
+    }
+
+    public static void main(String args[]) {
+
+        Node root = null;
+
+        root = insert(root, 50);
+        root = insert(root, 30);
+        root = insert(root, 20);
+        root = insert(root, 40);
+        root = insert(root, 70);
+        root = insert(root, 60);
+        root = insert(root, 80);
+
+        inOrder(root);
+
+    }
+}
